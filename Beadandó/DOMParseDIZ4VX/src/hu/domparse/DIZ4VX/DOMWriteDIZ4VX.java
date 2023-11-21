4d6d0e516b7bd5521899c234105ff18799f396a9
readywer <90259918+readywer@users.noreply.github.com>
2023-11-21 10:27:08 +0100

    fix

--- Beadandó/DOMParseDIZ4VX/src/hu/domparse/DIZ4VX/DOMWriteDIZ4VX.java
+++ Beadandó/DOMParseDIZ4VX/src/hu/domparse/DIZ4VX/DOMWriteDIZ4VX.java
@@ -1,27 +1,58 @@
 package hu.domparse.DIZ4VX;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 
 public class DOMWriteDIZ4VX {
     public static void main(String[] args) {
         try {
-            // XML-dokumentum beolvasása
-            File xmlFile = new File("XMLDIZ4VX.xml");
-            Document doc = DomReadDIZ4VX.parseXML(xmlFile);
+            // Új dokumentum létrehozása
+            Document doc = createSampleDocument();
 
             // A dokumentum fastruktúrájának kilistázása a konzolra
             System.out.println("Fa struktúra:");
             DomReadDIZ4VX.listNodes(doc.getDocumentElement(), "");
 
             // Az új XML-fájl elkészítése
-            File outputFile = new File("XMLDIZ4VX1.xml");
+            File outputFile = new File("XMLDIZ4VX2.xml");
             DomReadDIZ4VX.writeXML(doc, outputFile);
 
             System.out.println("A XMLDIZ4VX1.xml fájl elkészült.");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // Új dokumentum létrehozása előre meghatározott adatokkal
+    private static Document createSampleDocument() throws Exception {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.newDocument();
+
+        // Gyökér elem létrehozása
+        Element rootElement = doc.createElement("Fiók");
+        rootElement.setAttribute("FID", "1");
+        doc.appendChild(rootElement);
+
+        // Felhasználónév elem hozzáadása
+        Element felhasznalonevElement = doc.createElement("Felhasználónév");
+        felhasznalonevElement.setTextContent("user1");
+        rootElement.appendChild(felhasznalonevElement);
+
+        // Jelszó elem hozzáadása
+        Element jelszoElement = doc.createElement("Jelszó");
+        jelszoElement.setTextContent("password1");
+        rootElement.appendChild(jelszoElement);
+
+        // Kedvezmény elem hozzáadása
+        Element kedvezmenyElement = doc.createElement("Kedvezmény");
+        kedvezmenyElement.setTextContent("10");
+        rootElement.appendChild(kedvezmenyElement);
+
+        return doc;
     }
 }
