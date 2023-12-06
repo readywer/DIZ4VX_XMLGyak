26e287ea0f35c1e8f3444421c43144d11821c737
readywer <90259918+readywer@users.noreply.github.com>
2023-12-06 11:46:11 +0100

    jav, ora

---
+++ DIZ4VX_1206/JSONParseDIZ4VX/src/DIZ4VX/JSONRead.java
@@ -0,0 +1,15 @@
+package DIZ4VX;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JSONRead {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
